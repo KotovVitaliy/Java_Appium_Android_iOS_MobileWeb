30965eabf4016317a6442604239091bea60edfa1
vitaliy kotov <v.kotov@corp.badoo.com>
2019-09-10 15:31:31 +0300

    init

---
+++ src/test/java/lib/ui/ArticlePageObject.java
@@ -0,0 +1,141 @@
+package lib.ui;
+
+import org.openqa.selenium.WebElement;
+import lib.Platform;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+abstract public class ArticlePageObject extends MainPageObject
+{
+    protected static String
+        TITLE,
+        FOOTER_ELEMENT,
+        OPTIONS_BUTTON,
+        OPTIONS_ADD_TO_MY_LIST_BUTTON,
+        OPTION_REMOVE_FROM_MY_LIST_BUTTON,
+        ADD_TO_MY_LIST_OVERLAY,
+        MY_LIST_NAME_INPUT,
+        MY_LIST_OK_BUTTON,
+        CLOSE_ARTICLE_BUTTON;
+
+    public ArticlePageObject(RemoteWebDriver driver)
+    {
+        super(driver);
+    }
+
+    public WebElement waitForTitleElement()
+    {
+        return this.waitForElementPresent(TITLE, "Cannot find article title on page!", 15);
+    }
+
+    public String getArticleTitle()
+    {
+        WebElement title_element = waitForTitleElement();
+        if (Platform.getInstance().isAndroid()){
+            return title_element.getAttribute("text");
+        } else if (Platform.getInstance().isIOS()) {
+            return title_element.getAttribute("name");
+        } else {
+            return title_element.getText();
+        }
+    }
+
+    public void swipeToFooter()
+    {
+        if (Platform.getInstance().isAndroid()){
+            this.swipeUpToFindElement(
+                    FOOTER_ELEMENT,
+                    "Cannot find the end of article",
+                    40
+            );
+        } else if (Platform.getInstance().isIOS()) {
+            this.swipeUpTillElementAppear(
+                    FOOTER_ELEMENT,
+                    "Cannot find the end of article",
+                    40
+            );
+        } else {
+            this.scrollWebPageTillElementNotVisible(
+                    FOOTER_ELEMENT,
+                    "Cannot find the end of article",
+                    40
+            );
+        }
+    }
+
+    public void addArticleToMyList(String name_of_folder)
+    {
+        this.waitForElementAndClick(
+               OPTIONS_BUTTON,
+                "Cannot find button to open article options",
+                5
+        );
+
+        this.waitForElementAndClick(
+                OPTIONS_ADD_TO_MY_LIST_BUTTON,
+                "Cannot find option to add article to reading list",
+                5
+        );
+
+        this.waitForElementAndClick(
+                ADD_TO_MY_LIST_OVERLAY,
+                "Cannot find 'Got it' tip overlay",
+                5
+        );
+
+        this.waitForElementAndClear(
+                MY_LIST_NAME_INPUT,
+                "Cannot find input to set name of articles folder",
+                5
+        );
+
+        this.waitForElementAndSendKeys(
+                MY_LIST_NAME_INPUT,
+                name_of_folder,
+                "Cannot put text into articles folder input",
+                5
+        );
+
+        this.waitForElementAndClick(
+                MY_LIST_OK_BUTTON,
+                "Cannot press OK button",
+                5
+        );
+    }
+
+    public void addArticlesToMySaved()
+    {
+        if (Platform.getInstance().isMW()) {
+            this.removeArticleFromSavedIfItAdded();
+        }
+
+        this.waitForElementAndClick(OPTIONS_ADD_TO_MY_LIST_BUTTON, "Cannot find option to add article to reading list", 5);
+    }
+
+    public void removeArticleFromSavedIfItAdded()
+    {
+        if (this.isElementPresent(OPTION_REMOVE_FROM_MY_LIST_BUTTON)) {
+            this.waitForElementAndClick(
+                    OPTION_REMOVE_FROM_MY_LIST_BUTTON,
+                    "Cannot click button to remove an article from saved",
+                    1
+            );
+            this.waitForElementPresent(
+                    OPTIONS_ADD_TO_MY_LIST_BUTTON,
+                    "Cannot find button to add an article to saved list after removing it from this list before."
+            );
+        }
+    }
+
+    public void closeArticle()
+    {
+        if (Platform.getInstance().isIOS() || Platform.getInstance().isAndroid()) {
+            this.waitForElementAndClick(
+                    CLOSE_ARTICLE_BUTTON,
+                    "Cannot close article, cannot find X link",
+                    5
+            );
+        } else {
+            System.out.println("Method closeArticle() do nothing for platform " + Platform.getInstance().getPlatformVar());
+        }
+    }
+}
---
+++ src/test/java/tests/MyListsTests.java
@@ -0,0 +1,71 @@
+package tests;
+
+import lib.CoreTestCase;
+import lib.Platform;
+import lib.ui.*;
+import lib.ui.factories.ArticlePageObjectFactory;
+import lib.ui.factories.MyListsPageObjectFactory;
+import lib.ui.factories.NavigationUIFactory;
+import lib.ui.factories.SearchPageObjectFactory;
+import org.junit.Test;
+
+public class MyListsTests extends CoreTestCase
+{
+    private static final String name_of_folder = "Learning programming";
+    private static final String
+            login = "vinkotov",
+            password = "qazwsx";
+
+    @Test
+    public void testSaveFirstArticleToMyList()
+    {
+        SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
+
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSeachLine("Java");
+        SearchPageObject.clickByArticleWithSubstring("bject-oriented programming language");
+
+        ArticlePageObject ArticlePageObject = ArticlePageObjectFactory.get(driver);
+        ArticlePageObject.waitForTitleElement();
+        String article_title = ArticlePageObject.getArticleTitle();
+
+        if (Platform.getInstance().isAndroid()) {
+            ArticlePageObject.addArticleToMyList(name_of_folder);
+        } else {
+            ArticlePageObject.addArticlesToMySaved();
+        }
+
+        if (Platform.getInstance().isMW()) {
+            AuthorizationPageObject Auth = new AuthorizationPageObject(driver);
+            Auth.clickAuthButton();
+            Auth.enterLogInData(login, password);
+            Auth.submitForm();
+
+            // wait for redirect back to the title
+            ArticlePageObject.waitForTitleElement();
+
+            // check we back to the same page
+            assertEquals(
+                    "We back not to the same page after login.",
+                    article_title,
+                    ArticlePageObject.getArticleTitle()
+            );
+
+            ArticlePageObject.addArticlesToMySaved();
+        }
+
+        ArticlePageObject.closeArticle();
+
+        NavigationUI NavigationUI = NavigationUIFactory.get(driver);
+        NavigationUI.openNavigation();
+        NavigationUI.clickMyLists();
+
+        MyListsPageObject MyListsPageObject = MyListsPageObjectFactory.get(driver);
+
+        if (Platform.getInstance().isAndroid()){
+            MyListsPageObject.openFolderByName(name_of_folder);
+        }
+
+        MyListsPageObject.swipeByArticleToDelete(article_title);
+    }
+}
